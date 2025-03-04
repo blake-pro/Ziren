eda2e099875be7ef509429f3625e7c272d6ff56f
felicityin <yinjingping2022@gmail.com>
2025-03-04 21:24:30 +0800

    chore: septic test

--- crates/stark/src/septic_curve.rs
+++ crates/stark/src/septic_curve.rs
@@ -255,7 +255,6 @@ mod tests {
         ]);
         let (curve_point, _, _, _) = SepticCurve::<BabyBear>::lift_x(x);
         assert!(curve_point.check_on_point());
-        assert!(!curve_point.x.is_receive());
     }
 
     #[test]
